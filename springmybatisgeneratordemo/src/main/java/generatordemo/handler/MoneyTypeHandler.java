c9e3401fdf515b20da337f7b8088b69c0c472b21
luoli <2084267015@qq.com>
2019-06-08 15:17:09 +0800

    mybatis generator use

---
+++ springmybatisgeneratordemo/src/main/java/generatordemo/handler/MoneyTypeHandler.java
@@ -0,0 +1,44 @@
+package generatordemo.handler;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * money to Long,Long to money handler
+ * BaseTypeHandler:mybatis
+ */
+public class MoneyTypeHandler extends BaseTypeHandler<Money> {
+
+    //money to Long
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Money parameter, JdbcType jdbcType) throws SQLException {
+        ps.setLong(i, parameter.getAmountMinorLong());
+    }
+
+    @Override
+    public Money getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return parseMoney(rs.getLong(columnName));
+    }
+
+    @Override
+    public Money getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return parseMoney(rs.getLong(columnIndex));
+    }
+
+    @Override
+    public Money getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return parseMoney(cs.getLong(columnIndex));
+    }
+
+    //Long to money
+    private Money parseMoney(Long value) {
+        return Money.of(CurrencyUnit.of("CNY"), value / 100.0);
+    }
+}
