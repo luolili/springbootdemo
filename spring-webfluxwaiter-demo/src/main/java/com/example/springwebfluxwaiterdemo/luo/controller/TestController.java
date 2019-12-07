9e3201e5ac16a1cca5da525bebed0a56ba1e69c9
Administrator <2084267015@qq.com>
2019-12-07 17:45:24 +0800

    mybatis-plus

--- spring-webfluxwaiter-demo/src/main/java/com/example/springwebfluxwaiterdemo/luo/controller/TestController.java
+++ spring-webfluxwaiter-demo/src/main/java/com/example/springwebfluxwaiterdemo/luo/controller/TestController.java
@@ -3,9 +3,11 @@ package com.example.springwebfluxwaiterdemo.luo.controller;
 import com.example.springwebfluxwaiterdemo.luo.model.Coffee;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 @Slf4j
 @RestController
@@ -44,6 +46,27 @@ public class TestController {
         log.info("get1 st");
         Mono<String> res = Mono.fromSupplier(this::doSomething);
         log.info("get1 end");
+        return res;
+    }
+
+    /**
+     * flux 返回多个数据
+     *
+     * @return
+     */
+    //@GetMapping(path = "/3")//return: d:1d:2d:3d:4d:5
+    @GetMapping(path = "/3", produces = "text/event-stream")//return: d:1d:2d:3d:4d:5
+    public Flux<String> halo3() {
+        Flux<String> res = Flux.fromStream(IntStream.range(1, 6).mapToObj(
+                i -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return "d:" + i;
+                }
+        ));
         return res;
     }
 
