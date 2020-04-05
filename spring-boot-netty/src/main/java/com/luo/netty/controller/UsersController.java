a49053be3b5e156c0675c6a4c2d8a985dca5c023
Administrator <2084267015@qq.com>
2020-04-05 16:00:59 +0800

    netty

--- spring-boot-netty/src/main/java/com/luo/netty/controller/UsersController.java
+++ spring-boot-netty/src/main/java/com/luo/netty/controller/UsersController.java
@@ -3,23 +3,23 @@ package com.luo.netty.controller;
 import com.luo.netty.entity.Users;
 import com.luo.netty.service.UsersService;
 import com.luo.netty.util.Result;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 /**
  * (Users)表控制层
  *
- * @author makejava
  * @since 2020-04-05 11:33:42
  */
 @RestController
-@RequestMapping("users")
+@RequestMapping("u")
+@Slf4j
 public class UsersController {
-    /**
-     * 服务对象
-     */
+
     @Resource
     private UsersService usersService;
 
@@ -29,31 +29,37 @@ public class UsersController {
      * @param id 主键
      * @return 单条数据
      */
-    @GetMapping("selectOne")
+    @GetMapping("detail")
     public Users selectOne(String id) {
         return this.usersService.queryById(id);
     }
 
-    @PostMapping("registOrLogin")
-    public Result registOrLogin(@RequestBody Users user) {
-        if (StringUtils.isEmpty(user.getUsername()) ||
-                StringUtils.isEmpty(user.getPassword())) {
-            return Result.error();
-        }
-        boolean existUser = usersService.queryByUsername(user.getUsername());
-        if (existUser) {
-            //登陆
-            Users u = usersService.login(user.getUsername(), user.getPassword());
-            if (u == null) {
-                return Result.error("用户名或密码错误");
-            }
-        } else {
-            //注册
-            user.setFaceImgBig("");
-            user.setFaceImg("");
-            usersService.insert(user);
-        }
-        return Result.error();
+    @GetMapping("login")
+    public Result registOrLogin() {
+//        if (StringUtils.isEmpty(user.getUsername()) ||
+//                StringUtils.isEmpty(user.getPassword())) {
+//            return Result.error();
+//        }
+//        boolean existUser = usersService.queryByUsername(user.getUsername());
+//        if (existUser) {
+//            //登陆
+//            Users u = usersService.login(user.getUsername(), user.getPassword());
+//            if (u == null) {
+//                return Result.error("用户名或密码错误");
+//            }
+//        } else {
+//            //注册
+//            user.setFaceImgBig("");
+//            user.setFaceImg("");
+//            user.setQrcode("");
+//            Users res = usersService.insert(user);
+//            UserVo userVo = new UserVo();
+//            BeanUtils.copyProperties(res,userVo);
+//            return Result.success(userVo);
+//
+//        }
+        log.info("ii--------");
+        return Result.success(new Users());
     }
 
 }
---
+++ spring-boot-netty/src/main/java/com/luo/netty/vo/UserVo.java
@@ -0,0 +1,16 @@
+package com.luo.netty.vo;
+
+import lombok.Data;
+
+@Data
+public class UserVo {
+    private String id;
+
+    private String username;
+
+    private String faceImg;
+
+    private String faceImgBig;
+
+    private String qrcode;
+}
